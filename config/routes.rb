c27ec4ffe1de37ed7a0f2c486ffe505c766732d7
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-20 14:11:59 +0300

    Update application routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "home#index"
+  resources :users, only: [:new, :create]
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get 'password', to: 'passwords#edit'
+  patch 'password', to: 'passwords#update', as: :edit_password
 end
