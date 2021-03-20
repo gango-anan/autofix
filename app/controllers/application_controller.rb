eeac69b8f1eddaa685f0ea721051f0acb9c3d5a8
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-20 13:42:53 +0300

    Add method to require a user to sign in before accessing certain resources.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
       Current.user = User.find_by(id: session[:user_id])
     end
   end
+
+  def user_logged_in!
+    redirect_to sign_in_path, alert: 'To proceed, first sign in.' if Current.user.nil?
+  end
 end
