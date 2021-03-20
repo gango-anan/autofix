765ece3c16625c56a58591d2d6d7ce22d6dff23f
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-20 13:45:35 +0300

    Add a helper method to aid with rendering flash alerts and notices in the application.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_flash
+    if flash[:notice]
+      render partial: 'shared/notices'
+    elsif flash[:alert]
+      render partial: 'shared/alerts'
+    end
+  end
 end
