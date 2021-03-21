9656091a4714f7c8ed06fdab005afe8adc4566b8
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-22 00:24:32 +0300

    Add method to create default Groups when a user is create.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  after_create :create_default_groups
+  default_scope -> { order(:id) }
+
   validates :username, :password, presence: true
   validates :username, uniqueness: true
   validates :username, length: { within: 4..20 }
@@ -7,4 +10,18 @@ class User < ApplicationRecord
 
   has_many :expenditures, dependent: :destroy
   has_many :groups, dependent: :destroy
+
+  private
+  
+  def create_default_groups
+    Group.create(name: 'Tyre-Clinic', user_id: id)
+    Group.create(name: 'Service', user_id: id)
+    Group.create(name: 'Body', user_id: id)
+    Group.create(name: 'Engine', user_id: id)
+    Group.create(name: 'Diagnostics', user_id: id)
+    Group.create(name: 'Electronics', user_id: id)
+    Group.create(name: 'Brakes', user_id: id)
+    Group.create(name: 'Suspension', user_id: id)
+    Group.create(name: 'Transmission', user_id: id)
+  end
 end
