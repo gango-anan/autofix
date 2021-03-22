4aea7bf99fa0e3655edd20d60fae7dbf4870ec58
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-22 03:42:14 +0300

    Add functionality to create a new group and display all groups attached to a user.

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,30 @@
+class GroupsController < ApplicationController
+  def index
+    @groups = Current.user.groups
+  end
+
+  def new
+    @group = Current.user.groups.build
+  end
+
+  def show
+  end
+
+  def create
+    group_params[:name].downcase!
+    @group = Current.user.groups.build(group_params)
+    if Current.user.groups.pluck(:name).include?(group_params[:name])
+      flash[:alert] = 'Group already exists try another name.'
+      render :new
+    else
+      @group.save
+      redirect_to user_path(Current.user.id), notice: 'Group Successfully added.'
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
+  end
+end
