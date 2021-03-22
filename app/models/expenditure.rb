178b1bde0f7b8a862f6ef8712caebd594071b741
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-22 17:37:05 +0300

    Add scopes to help filter the expenditures with groups and those without groups.

--- app/models/expenditure.rb
+++ app/models/expenditure.rb
@@ -1,6 +1,7 @@
 class Expenditure < ApplicationRecord
-  default_scope -> { order(:id) }
-  
+  default_scope -> { order(created_at: :desc) }
+  scope :un_grouped_expenditures, -> { where(group_id: nil) }
+  scope :grouped_expenditures, -> { where.not(group_id: nil) }
   validates :name, presence: true
   validates :amount, presence: true
   validates :author_id, presence: true
