d98f5a50d5ec7f96237cdf0041a0c146936755c0
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-26 15:45:14 +0300

    Remove unused routes.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   get 'password', to: 'passwords#edit'
   patch 'password', to: 'passwords#update', as: :edit_password
-  resources :groups
-  resources :expenditures
+  resources :groups, except: %i[edit update]
+  resources :expenditures, only: %i[index new create]
   get 'external_expenditures', to: 'expenditures#other_expenditures'
 end
